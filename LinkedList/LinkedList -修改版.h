fb38d1212dbad2cb01120a0bac997aee105d2f02
xueyicheng1026 <1483006233@qq.com>
2024-10-12 12:21:50 +0800

    LinkedList提交

---
+++ LinkedList/LinkedList -修改版.h
@@ -0,0 +1,237 @@
+#include <initializer_list>
+#include <iostream>
+
+template <typename T>
+class SingleLinkedList
+{
+private:
+    class Node
+    {
+    private:
+        T data;
+        Node *next = nullptr;
+        Node(T _val){data = _val;}
+
+        friend class SingleLinkedList<T>;
+    };
+    Node* head = nullptr;
+    int size = 0;
+    Node* currentPos = nullptr;
+    Node* precur = nullptr;
+    void _emptyList();
+    void _copy(const SingleLinkedList<T> &_l);
+public:
+    /// 返回当前位置的值
+    T getCurrentVal() const;
+    /// 设置当前位置的值
+    void setCurrentVal(const T &_val);
+    /// 如果链表为空，返回 true；否则返回 false
+    bool isEmpty() const;
+
+    int getSize() const;
+    void emptyList();
+    bool find (const T &_val);
+    SingleLinkedList(){};
+    ~SingleLinkedList(); 
+    SingleLinkedList(std::initializer_list<T> _l);
+    void printList() const;
+    SingleLinkedList(const SingleLinkedList<T> &_l);
+    SingleLinkedList<T>& operator=(const SingleLinkedList<T> &_l);
+
+    /// 在 currentPos 后面插入一个元素，数据为 _val
+    void insert(T _val); 
+    /// 如果找到，返回 ture, currentPos 停留在第一个 _val 的位置。
+    /// 否则返回 false, currentPos 不动。
+    bool find(const T &_val) const;  
+    /// 删除 currentPos 后面的元素
+    void remove();                                        
+};
+
+template<typename T>
+bool SingleLinkedList<T>::find(const T &_val)
+{
+    Node *p = head;
+    precur = p;
+    while (p != nullptr)
+    {
+        if (p->data == _val)
+        {
+            currentPos = p;
+            return true;
+        }
+        precur = p;
+        p = p->next;    
+    }
+    return false;
+}
+
+template<typename T>
+int SingleLinkedList<T>::getSize() const
+{
+    return size;
+}
+
+template <typename T>
+SingleLinkedList<T>::SingleLinkedList(std::initializer_list<T> _l)
+{
+    for (auto i = _l.begin(); i != _l.end(); ++i)
+    {
+        Node* newNode = new Node(*i);
+        if (head == nullptr)
+        {
+            head = newNode;
+            precur = head;
+            currentPos = head;
+        }
+        else
+        {
+            precur = currentPos;
+            currentPos->next = newNode;
+            currentPos = newNode;
+        }
+        ++size;
+    }
+}
+
+template <typename T>
+void SingleLinkedList<T>::_emptyList()
+{
+    Node* p = head;
+    while (p != nullptr)
+    {
+        Node* t = p;
+        p = p->next; 
+        delete t;
+    }
+}
+
+template <typename T>
+void SingleLinkedList<T>::printList() const
+{
+    Node* p = head;
+    while (p != nullptr)
+    {
+        std::cout << p->data << "\t";
+        p = p->next;
+    }
+    std::cout << std::endl;    
+}
+
+template <typename T>
+SingleLinkedList<T>::~SingleLinkedList()
+{
+    _emptyList();
+};
+
+template<typename T>
+SingleLinkedList<T>::SingleLinkedList(const SingleLinkedList<T> &_l)
+{
+    _copy(_l);
+}
+
+template<typename T>
+void SingleLinkedList<T>::emptyList()
+{
+    _emptyList();
+    head = nullptr;
+    precur = nullptr;
+    currentPos = nullptr;
+    size = 0;
+}
+
+template<typename T>
+SingleLinkedList<T>& SingleLinkedList<T>::operator=(const SingleLinkedList<T> &_l)
+{
+    if (this == &_l)
+        return *this;
+    emptyList();
+    _copy(_l);
+    return *this;
+}
+
+template <typename T>
+void SingleLinkedList<T>::_copy(const SingleLinkedList<T> &_l)
+{
+    Node *p = _l.head;
+    while (p != nullptr)
+    {
+        Node* newNode = new Node(p->data);
+        if (head == nullptr)
+        {
+            head = newNode;
+            precur = head;
+            currentPos = head;
+        }
+        else
+        {
+            precur = currentPos;
+            currentPos->next = newNode;
+            currentPos = newNode;
+        }
+        ++size;
+        p = p->next;
+    }
+}
+
+template <typename T>
+T SingleLinkedList<T>::getCurrentVal() const
+{
+    if (currentPos == nullptr)
+    {
+        std::cout << "Empty current position! Can't get value!" << std::endl;
+        exit(0);
+    }
+    return currentPos->data;
+}
+
+template <typename T>
+void SingleLinkedList<T>::setCurrentVal(const T &_val)
+{
+    if (currentPos == nullptr)
+    {
+        std::cout << "Empty current position! Can't set value!" << std::endl;
+        exit(0);
+    }
+    currentPos->data = _val;
+}
+
+template <typename T>
+bool SingleLinkedList<T>::isEmpty() const
+{
+    return size == 0 ? true : false;
+}
+
+template <typename T>
+void SingleLinkedList<T>::insert(T _val)
+{
+    if (isEmpty())
+    {
+        Node* newNode = new Node(_val);
+        head = newNode;
+        precur = newNode;
+        currentPos = newNode;
+    }
+    else
+    {
+        Node* newnode = new Node(_val);
+        newnode->next = currentPos->next;
+        currentPos->next = newnode;
+        precur = currentPos;
+        currentPos = newnode;
+    }
+   ++size;
+}
+
+template <typename T>
+void SingleLinkedList<T>::remove()
+{   
+    if (currentPos == nullptr) return;
+    if (size == 1)
+        emptyList();
+    else
+    {
+        precur->next = currentPos->next;
+        delete currentPos;
+    }
+    --size;
+}
